ebde5ca0c0ed31548e48a0e27ae2df3a9b95fbf9
Eze <ezekiel.kuang@gmail.com>
2026-05-19 19:48:11 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,29 +1,9 @@
-const CACHE = 'mesmerize-v1';
-const ASSETS = [
-  '/',
-  '/index.html',
-  '/1.mp4',
-  '/3.mp4',
-];
-
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE).then(c => c.addAll(ASSETS))
-  );
-  self.skipWaiting();
-});
-
+// This service worker intentionally does nothing and unregisters itself.
+// It was removed because it was intercepting video requests with an empty cache.
+self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', e => {
   e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    )
-  );
-  self.clients.claim();
-});
-
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(cached => cached || fetch(e.request))
+    caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
+      .then(() => self.registration.unregister())
   );
 });
