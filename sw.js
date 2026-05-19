056e62e68a8cb73944334ee2e2908c18ee70ba50
Eze <ezekiel.kuang@gmail.com>
2026-05-19 19:35:55 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,29 @@
+const CACHE = 'mesmerize-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/1.mp4',
+  '/3.mp4',
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
+});
